f880a44ab5a3a3e8ead634fd9da1c39660a3af37
Alejandro <arecoalejandro@gmail.com>
2024-08-08 16:39:33 -0300

    practica conversor de monedas en py

---
+++ conversor-moneda.py
@@ -0,0 +1,26 @@
+#Paso 1: Definir valor actual del Euro y Dolar con respecto al peso mexicano
+
+tipo_cambio_eur_a_mxn = 23.70 #Este valor esta hardcodeado ya que es una ejercitacion. Deberia ir a buscar el valor a una BBDD o API
+tipo_cambio_usd_a_mxn = 20.75 #Este valor esta hardcodeado ya que es una ejercitacion. Deberia ir a buscar el valor a una BBDD o API
+
+#Paso 2: Solicitar el usuario el tipo de conversion (Euro a Mex o Dolar a Mex).
+    
+tipo_conversion = input("Ingrese la moneda origen para la conversion (EUR/USD): ")
+
+#Paso 3: Solicitar al usuario el monto a convertir.
+
+monto_a_convertir = float (input("Ingrese el monto a convertir: "))
+
+
+#Paso 4: Realizar la conversion utilizando el tipo de cambio correspondiente
+#Paso 5: Mostrar el resultado de la conversion al usuario.
+
+
+if tipo_conversion.upper() == "EUR":
+    resultado = monto_a_convertir * tipo_cambio_eur_a_mxn
+    print("El resultado de la conversion de EUR a MXN es: ", resultado)
+elif tipo_conversion.upper() == "USD":
+    resultado = monto_a_convertir * tipo_cambio_usd_a_mxn
+    print("El resultado de la conversion de USD a MXN es: ", resultado)
+else:   
+    print("No esta disponible este tipo de conversion actualmente")
